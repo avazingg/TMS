d1a3391b28140e4557e6604316d719a6191ddf3d
avazingg <avazingg@gmail.com>
2024-11-08 17:19:52 +0100

    update/hw4-logical-operators-in-progress

---
+++ HW4/logical_operators.py
@@ -0,0 +1,19 @@
+# logical_operators
+
+# 1
+a = 14
+b = 73
+
+# 2
+c = (a > 13 and b > 72)
+d = (a < 100 and b < 100)
+e = (a > 14 and b > 72)
+f = (isinstance(a, float) and isinstance(b, int))
+print (c, d, e, f)
+
+# 3
+g = (a > 10 or b > 80)
+h = (isinstance(a, int) or isinstance(b, int))
+l = (a > 14 or b >73 )
+m = (isinstance(a, float) or isinstance(b, float))
+print (g, h, l, m)
