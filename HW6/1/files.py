2709d6b83c8bf7d1254bc3fd10483bfc1b592c38
avazingg <avazingg@gmail.com>
2024-11-21 15:53:33 +0100

    hw5-completed

--- HW6/1/files.py
+++
@@ -1,9 +0,0 @@
-with open('nums.txt', 'r') as file:
-    for line in file:
-        if len(line.split(" ")) < 4:
-            print("Error: length of first line is lees than 4 elements")
-        else:
-            for number in line.split(" "):
-                print(number)
-
-
