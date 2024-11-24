c37bfe92842b884f11a568b66af8edd5ec490431
Vasili <avazingg@gmail.com>
2024-11-24 18:06:35 +0100

    HW5-added-new-function-with-args-and-kwargs

--- HW5/def.py
+++ HW5/def.py
@@ -19,19 +19,16 @@ def find_week_day_by_number(day):
 
 print(find_week_day_by_number(2))
 
-
-
 def unique_numbers_in_list(arr):
     unique = ""
-    for i in range(arr[0], len(arr)):
+    for i in range(len(arr)):
         if arr.count(arr[i]) == 1:
             unique += str(arr[i]) + " "
     return unique.strip()
 
-
 list0 = [1, 4, 2, 3, 5, 1, 3, 2, 4, 10]
 list1 = [4, 5, 6, 7, -2, 1, -3, 5, -10, 15]
-print(unique_numbers_in_list(list))
+print(unique_numbers_in_list(list0))
 
 
 def sum_of_lists(a,b):
@@ -43,3 +40,11 @@ def sorted_sum_of_2_arrays(a,b):
     return sorted(sum_of_lists(a,b))
 
 print(sorted_sum_of_2_arrays(list0,list1))
+
+def greetings(*args, **kwargs):
+    for name in args:
+        print(f"Hello, {name}")
+    for key, value in kwargs.items():
+        print(f"{key.upper()}: {value}")
+
+greetings("Dwight", "Michael", "Jim", series = "office", rating=10 )
