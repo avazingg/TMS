ed29b0dfa4d7869e4ae167d71a2bc5064500d9a6
Vasili <avazingg@gmail.com>
2024-11-27 21:21:53 +0100

    HW8-3/4-tasks-completed

--- HW8/Factorial.py
+++ HW8/Factorial.py
@@ -1,14 +1,20 @@
 import time
+
+def decorator(func):
+    def wrapper(n):
+        start_time = time.time()
+        result = func(n)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Elapsed time: {elapsed_time:.6f} seconds")
+        return result
+    return wrapper
+
 def recursion_factorial(n):
     if n == 0:
         return 1
     else:
         return n * recursion_factorial(n-1)
-
-start_time = time.time()
-result = recursion_factorial(10)
-end_time = time.time()
-elapsed_time = end_time - start_time
 
 def generator_factorial(n):
     result = 1
@@ -16,20 +22,13 @@ def generator_factorial(n):
         result *= number
     yield result
 
-start_time1 = time.time()
-result1 = generator_factorial(10)
-end_time1 = time.time()
-elapsed_time1 = end_time1 - start_time1
+@decorator
+def compute_recursion_factorial(n):
+    return recursion_factorial(n)
 
-print("\nresult for recursion:", recursion_factorial(20), "\nelapsed time for recursion", elapsed_time)
-print("\nresult for genereator:", next(generator_factorial(20)), "\nelapsed time for genereator", elapsed_time1)
+@decorator
+def compute_generator_factorial(n):
+    return  next(generator_factorial(n))
 
-print("\nGenerator works faster than recursion:" ,elapsed_time > elapsed_time1)
-
-
-# def calculate_time(func, *args):
-#     start_time = time.time()
-#     result = func(*args)
-#     end_time = time.time()
-#     elapsed_time = end_time - start_time
-#     return result, elapsed_time
+print(compute_recursion_factorial(100))
+print(compute_generator_factorial(100))
